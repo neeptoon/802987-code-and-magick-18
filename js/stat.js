c94a2ee7df987e662f440728057b4308700295c0
Шеханов Евгений <neeptoon@mail.ru>
2019-09-15 22:24:45 +0300

    Добавляет функцию отрисовки облака

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,17 @@
-'use strict'
+'use strict';
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 110;
+var CLOUD_Y = 20;
+var GAP = 10;
 
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
-
-window.renderStatistics = function () {
-
-}
+window.renderStatistics = function (ctx) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
+};
 

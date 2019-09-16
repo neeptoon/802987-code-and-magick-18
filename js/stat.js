1f035a97420820190380cc818834f9604d68310d
Шеханов Евгений <neeptoon@mail.ru>
2019-09-16 06:41:14 +0300

    Добавляет отрисовку гистограммы линией

--- js/stat.js
+++ js/stat.js
@@ -11,7 +11,24 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx) {
+
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
+
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillStyle = '#000';
+  ctx.fillText('Ура Вы победили!', 140, 40);
+  ctx.fillText('Список результатов:', 140, 60);
+
+  ctx.fillText('вы', 140, 260);
+
+  ctx.strokeStyle = 'red';
+
+  ctx.lineWidth = 40;
+  ctx.moveTo(140, 250);
+  ctx.lineTo(140, 80);
+  ctx.stroke();
+  ctx.strokeStyle = 'blue';
 };
 

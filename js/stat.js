83b8cc7e4d2dd67064f1206883255f32428625bf
Шеханов Евгений <neeptoon@mail.ru>
2019-09-16 22:43:25 +0300

    Добавляет однотонную отрисовку гистограммы из функции с учетом времени

--- js/stat.js
+++ js/stat.js
@@ -1,34 +1,62 @@
 'use strict';
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var CLOUD_X = 110;
-var CLOUD_Y = 20;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
 var GAP = 10;
+var COLUMN_GAP = 30;
+var TEXT_HEIGHT = 20;
+var LEFT_MARGIN = 50;
+var BOTTOM_MARGIN = 20;
+var COLUMN_WIDTH = 40;
+var COLUMN_HEIGHT = 150;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function (ctx) {
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
 
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, 'white');
 
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура Вы победили!', 140, 40);
-  ctx.fillText('Список результатов:', 140, 60);
+  ctx.fillText('Ура вы победили!', CLOUD_X + LEFT_MARGIN, CLOUD_Y + GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + LEFT_MARGIN, CLOUD_Y + GAP + TEXT_HEIGHT);
 
-  ctx.fillText('вы', 140, 260);
+  var maxTime = getMaxElement(times);
 
-  ctx.strokeStyle = 'red';
+  // ctx.fillStyle = 'rgba(255, 0, 0)';
+  // ctx.fillText('Вы', CLOUD_X + LEFT_MARGIN, CLOUD_HEIGHT - BOTTOM_MARGIN);
+  // ctx.fillRect(CLOUD_X + LEFT_MARGIN, CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
 
-  ctx.lineWidth = 40;
-  ctx.moveTo(140, 250);
-  ctx.lineTo(140, 80);
-  ctx.stroke();
-  ctx.strokeStyle = 'blue';
+  // ctx.fillStyle = 'hsl(245, ' + ( 30 + 60 * Math.random()) + '%, 50%)';
+  // ctx.fillText('Илья', CLOUD_X + LEFT_MARGIN + COLUMN_WIDTH + COLUMN_GAP, CLOUD_HEIGHT - BOTTOM_MARGIN);
+  // ctx.fillRect(CLOUD_X + LEFT_MARGIN + COLUMN_WIDTH + COLUMN_GAP, CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
+
+  // ctx.fillStyle = 'hsl(245, ' + (30 + 60 * Math.random()) + '%, 50%)';
+  // ctx.fillText('Пётр', CLOUD_X + LEFT_MARGIN + 2 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - BOTTOM_MARGIN);
+  // ctx.fillRect(CLOUD_X + LEFT_MARGIN + 2 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
+
+  // ctx.fillStyle = 'hsl(245, ' + (30 + 60 * Math.random()) + '%, 50%)';
+  // ctx.fillText('Олег', CLOUD_X + LEFT_MARGIN + 3 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - BOTTOM_MARGIN);
+  // ctx.fillRect(CLOUD_X + LEFT_MARGIN + 3 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
+
+  for (var i = 0; i < names.length; i++) {
+    ctx.fillText(names[i], CLOUD_X + LEFT_MARGIN + i * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - BOTTOM_MARGIN);
+    ctx.fillRect(CLOUD_X + LEFT_MARGIN + i * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, - (COLUMN_HEIGHT * times[i] / maxTime));
+  }
 };
-

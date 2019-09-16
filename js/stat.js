e9dcc1cf2c673734b833c6548e52cb0cb95e034c
Шеханов Евгений <neeptoon@mail.ru>
2019-09-16 23:21:23 +0300

    Добавляет цвет при отрисовке гистограммы

--- js/stat.js
+++ js/stat.js
@@ -10,13 +10,14 @@ var LEFT_MARGIN = 50;
 var BOTTOM_MARGIN = 20;
 var COLUMN_WIDTH = 40;
 var COLUMN_HEIGHT = 150;
+var mainColor = 'rgba(255, 0, 0)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -39,24 +40,11 @@ window.renderStatistics = function (ctx, names, times) {
 
   var maxTime = getMaxElement(times);
 
-  // ctx.fillStyle = 'rgba(255, 0, 0)';
-  // ctx.fillText('Вы', CLOUD_X + LEFT_MARGIN, CLOUD_HEIGHT - BOTTOM_MARGIN);
-  // ctx.fillRect(CLOUD_X + LEFT_MARGIN, CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
-
-  // ctx.fillStyle = 'hsl(245, ' + ( 30 + 60 * Math.random()) + '%, 50%)';
-  // ctx.fillText('Илья', CLOUD_X + LEFT_MARGIN + COLUMN_WIDTH + COLUMN_GAP, CLOUD_HEIGHT - BOTTOM_MARGIN);
-  // ctx.fillRect(CLOUD_X + LEFT_MARGIN + COLUMN_WIDTH + COLUMN_GAP, CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
-
-  // ctx.fillStyle = 'hsl(245, ' + (30 + 60 * Math.random()) + '%, 50%)';
-  // ctx.fillText('Пётр', CLOUD_X + LEFT_MARGIN + 2 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - BOTTOM_MARGIN);
-  // ctx.fillRect(CLOUD_X + LEFT_MARGIN + 2 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
-
-  // ctx.fillStyle = 'hsl(245, ' + (30 + 60 * Math.random()) + '%, 50%)';
-  // ctx.fillText('Олег', CLOUD_X + LEFT_MARGIN + 3 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - BOTTOM_MARGIN);
-  // ctx.fillRect(CLOUD_X + LEFT_MARGIN + 3 * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -140);
-
   for (var i = 0; i < names.length; i++) {
+    var secondColor = 'hsl(245, ' + (25 + Math.random() * (75 + 1 - 25)) + '%, 50%)';
+    ctx.fillStyle = '#000';
     ctx.fillText(names[i], CLOUD_X + LEFT_MARGIN + i * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - BOTTOM_MARGIN);
-    ctx.fillRect(CLOUD_X + LEFT_MARGIN + i * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, - (COLUMN_HEIGHT * times[i] / maxTime));
+    ctx.fillStyle = names[i] === 'Вы' ? mainColor : secondColor;
+    ctx.fillRect(CLOUD_X + LEFT_MARGIN + i * (COLUMN_WIDTH + COLUMN_GAP), CLOUD_HEIGHT - 2 * BOTTOM_MARGIN, COLUMN_WIDTH, -(COLUMN_HEIGHT * times[i] / maxTime));
   }
 };

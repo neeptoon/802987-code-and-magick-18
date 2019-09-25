664068a517430b81a8c63a5b6f002eb72c87d448
Шеханов Евгений <neeptoon@mail.ru>
2019-09-25 07:46:44 +0300

    Добавляет открытие-закрытие окна setup по клику

--- js/setup.js
+++ js/setup.js
@@ -50,5 +50,17 @@ var renderWizards = function () {
   }
   return similarListElement.appendChild(fragment);
 };
-
 renderWizards();
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+
+setupOpen.addEventListener('click', function () {
+  setup.classList.remove('hidden');
+});
+
+setupClose.addEventListener('click', function () {
+  setup.classList.add('hidden');
+});
+

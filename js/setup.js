9158bcd8203c348dbeba92149c66e8210e634c7c
Шеханов Евгений <neeptoon@mail.ru>
2019-09-29 14:36:11 +0300

    Добавляет обработчики событий и валидацию формы только при открытии окна настроек

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,24 @@
 'use strict';
 
+var ESC_CODE = 27;
+var ENTER_CODE = 13;
+var amountWizardsRender = 4;
+var setupWindow = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setupWindow.querySelector('.setup-close');
+var setupUserName = setupWindow.querySelector('.setup-user-name');
+var setupPlayer = document.querySelector('.setup-player');
+var setupWizard = setupPlayer.querySelector('.setup-wizard');
+var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var wizardCoatInput = setupPlayer.querySelector('input[name="coat-color"]');
+var wizardEyesInput = setupPlayer.querySelector('input[name="eyes-color"]');
+var wizardFireball = setupPlayer.querySelector('.setup-fireball-wrap');
+var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var wizardFireballInput = wizardFireball.querySelector('input[name="fireball-color"]');
+var setupSimilarWindow = setupWindow.querySelector('.setup-similar');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var similarListElement = setupSimilarWindow.querySelector('.setup-similar-list');
+
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var colors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -9,7 +28,6 @@ var fireballs = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var getRandomNumber = function (min, max) {
   return Math.round(min - 0.5 + Math.random() * max - min + 1);
 };
-var amountWizardsRender = 4;
 
 var getWizards = function () {
   var wizardsList = [];
@@ -29,11 +47,6 @@ var getWizards = function () {
 
 var wizards = getWizards();
 
-var setupWindow = document.querySelector('.setup');
-var setupSimilarWindow = setupWindow.querySelector('.setup-similar');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var similarListElement = setupSimilarWindow.querySelector('.setup-similar-list');
-
 var getWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
@@ -51,13 +64,7 @@ var renderWizards = function () {
 };
 renderWizards();
 
-var ESC_CODE = 27;
-var ENTER_CODE = 13;
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = setupWindow.querySelector('.setup-close');
-var setupUserName = setupWindow.querySelector('.setup-user-name');
-
-var escKeydownHandler = function (evt) {
+var documentKeydownHandler = function (evt) {
   if (evt.target === setupUserName) {
     evt.stopPropagation();
   } else if (evt.keyCode === ESC_CODE) {
@@ -65,22 +72,42 @@ var escKeydownHandler = function (evt) {
   }
 };
 
-var addEscDownListener = function () {
-  document.addEventListener('keydown', escKeydownHandler);
+var setupCloseKeydownHandler = function (evt) {
+  if (evt.keyCode === ENTER_CODE) {
+    closePopup();
+  }
 };
 
-var removeEscDownListener = function () {
-  document.removeEventListener('keydown', escKeydownHandler);
+var setupCloseClickHandler = function () {
+  closePopup();
 };
 
 var openPopup = function () {
   setupWindow.classList.remove('hidden');
-  addEscDownListener();
+  document.addEventListener('keydown', documentKeydownHandler);
+  setupClose.addEventListener('click', setupCloseClickHandler);
+  setupClose.addEventListener('keydown', setupCloseKeydownHandler);
+  setupUserName.addEventListener('invalid', checkForm);
 };
 
 var closePopup = function () {
   setupWindow.classList.add('hidden');
-  removeEscDownListener();
+  document.removeEventListener('keydown', documentKeydownHandler);
+  setupClose.removeEventListener('click', setupCloseClickHandler);
+  setupClose.removeEventListener('keydown', setupCloseKeydownHandler);
+  setupUserName.removeEventListener('invalid', checkForm);
+};
+
+var checkForm = function () {
+  if (setupUserName.validity.tooShort) {
+    setupUserName.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (setupUserName.validity.tooLong) {
+    setupUserName.setCustomValidity('Палехче, приятель!!');
+  } else if (setupUserName.validity.valueMissing) {
+    setupUserName.setCustomValidity('Заполни поле');
+  } else {
+    setupUserName.setCustomValidity('');
+  }
 };
 
 setupOpen.addEventListener('click', function () {
@@ -92,37 +119,6 @@ setupOpen.addEventListener('keydown', function (evt) {
     openPopup();
   }
 });
-
-setupClose.addEventListener('click', function () {
-  closePopup();
-});
-
-setupClose.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === ENTER_CODE) {
-    closePopup();
-  }
-});
-
-setupUserName.addEventListener('invalid', function () {
-  if (setupUserName.validity.tooShort) {
-    setupUserName.setCustomValidity('Имя должно состоять минимум из 2-х символов');
-  } else if (setupUserName.validity.tooLong) {
-    setupUserName.setCustomValidity('Палехче, приятель!!');
-  } else if (setupUserName.validity.valueMissing) {
-    setupUserName.setCustomValidity('Заполни поле');
-  } else {
-    setupUserName.setCustomValidity('');
-  }
-});
-
-var setupPlayer = document.querySelector('.setup-player');
-var setupWizard = setupPlayer.querySelector('.setup-wizard');
-var wizardCoat = setupWizard.querySelector('.wizard-coat');
-var wizardCoatInput = setupPlayer.querySelector('input[name="coat-color"]');
-var wizardEyesInput = setupPlayer.querySelector('input[name="eyes-color"]');
-var wizardFireball = setupPlayer.querySelector('.setup-fireball-wrap');
-var wizardEyes = setupWizard.querySelector('.wizard-eyes');
-var wizardFireballInput = wizardFireball.querySelector('input[name="fireball-color"]');
 
 var changeParam = function (arr) {
   return arr[getRandomNumber(0, arr.length - 1)];
